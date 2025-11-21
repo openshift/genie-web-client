39866bed7aeb3d075edf415e0ff04da26952a640
Ivana Rodriguez <ivrodrig@redhat.com>
2025-11-21 14:10:27 -0500

    fix: import missing styles

--- src/components/GeniePage.tsx
+++ src/components/GeniePage.tsx
@@ -1,5 +1,6 @@
 import { DrawerProvider } from './global-drawer';
 import { Layout } from './global-layout/Layout';
+import '@patternfly/react-core/dist/styles/base.css';
 import '@patternfly/chatbot/dist/css/main.css';
 import './genie.css';
 
