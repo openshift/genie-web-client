adfadaea22605a986d237f7cf3bc235b90396631
Ivana Rodriguez <ivrodrig@redhat.com>
2025-12-02 15:48:10 -0500

    refactor(onboarding): simplify component and tests

--- src/components/GeniePage.tsx
+++ src/components/GeniePage.tsx
@@ -1,6 +1,6 @@
 import { DrawerProvider } from './global-drawer';
 import { Layout } from './global-layout/Layout';
-import { OnboardingProvider, OnboardingModal } from './onboarding';
+import { OnboardingModal } from './onboarding';
 import '@patternfly/react-core/dist/styles/base.css';
 import '@patternfly/chatbot/dist/css/main.css';
 import './genie.css';
@@ -8,12 +8,10 @@ import './genie.css';
 export default function GeniePage() {
   return (
     <div className="global-layout-container">
-      <OnboardingProvider>
-        <DrawerProvider>
-          <Layout />
-        </DrawerProvider>
-        <OnboardingModal />
-      </OnboardingProvider>
+      <DrawerProvider>
+        <Layout />
+      </DrawerProvider>
+      <OnboardingModal />
     </div>
   );
 }
--- src/components/onboarding/index.ts
+++ src/components/onboarding/index.ts
@@ -1,6 +1,1 @@
-export { OnboardingContext, useOnboarding } from './OnboardingContext';
-export type { OnboardingState, OnboardingContextValue } from './OnboardingContext';
-export { OnboardingProvider } from './OnboardingProvider';
 export { OnboardingModal } from './OnboardingModal';
-export { ONBOARDING_PAGES } from './onboarding-content';
-export type { DeckButton, DeckPage } from './onboarding-content';
