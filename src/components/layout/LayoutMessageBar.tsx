a5995ac582bde5b591d07232da23d309e28e02f9
Kim Doberstein <kdoberst@redhat.com>
2026-01-28 15:03:14 -0600

    GIE-408 Fix issue where every user message was starting a new conversation

--- src/components/layout/LayoutMessageBar.tsx
+++ src/components/layout/LayoutMessageBar.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { useCallback, type FC } from 'react';
 import { useNavigate } from 'react-router-dom-v5-compat';
 import { MessageBar } from '@patternfly/chatbot';
-import { useCreateNewConversation, useSendStreamMessage } from '../../hooks/AIState';
+import {
+  useCreateNewConversation,
+  useSendStreamMessage,
+  useActiveConversation,
+} from '../../hooks/AIState';
 import { mainGenieRoute, SubRoutes } from '../routeList';
 
 interface LayoutMessageBarProps {
@@ -13,14 +17,18 @@ export const LayoutMessageBar: FC<LayoutMessageBarProps> = ({ messageBarRef }) =
   const navigate = useNavigate();
   const sendStreamMessage = useSendStreamMessage();
   const createNewConversation = useCreateNewConversation();
+  const activeConversation = useActiveConversation();
 
   const handleSendMessage = useCallback(
     async (value: string | number) => {
-      await createNewConversation();
+      // Only create a new conversation if there isn't an active one
+      if (!activeConversation) {
+        await createNewConversation();
+      }
       sendStreamMessage(value);
       navigate(`${mainGenieRoute}/${SubRoutes.Chat}`);
     },
-    [sendStreamMessage, navigate, createNewConversation],
+    [sendStreamMessage, navigate, createNewConversation, activeConversation],
   );
 
   return <MessageBar ref={messageBarRef} onSendMessage={handleSendMessage} />;
